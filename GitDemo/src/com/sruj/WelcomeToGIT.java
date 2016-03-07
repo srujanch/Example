b672853e5bad0e3e28e69ba323ea605c28b11211
schimaku <schimaku@SCHIMAKU3JWW5Z1.opentext.net>
2016-03-07 15:27:01 +0530

    Intial Commit

---
+++ GitDemo/src/com/sruj/WelcomeToGIT.java
@@ -0,0 +1,14 @@
+package com.sruj;
+
+public class WelcomeToGIT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to GIT");
+
+	}
+
+}
